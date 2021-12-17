12cf122313be2b86cee46bb7018e10a013510ebe
Kishore Annam <annamkishore@gmail.com>
2021-12-18 02:36:52 +0530

    Updates

---
+++ precache-manifest.2f906a24b4218f331b5fc42586f29cf8.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ee69ca705cd491e0e9b24a6195a8a7",
+    "url": "/bhagavad-gita/index.html"
+  },
+  {
+    "revision": "a04881038ee41e7d9849",
+    "url": "/bhagavad-gita/static/css/main.f7aa5998.chunk.css"
+  },
+  {
+    "revision": "f2d433d199f86ecb2400",
+    "url": "/bhagavad-gita/static/js/2.e1c013c9.chunk.js"
+  },
+  {
+    "revision": "d83b5f4725283a14ac4560dc883dee04",
+    "url": "/bhagavad-gita/static/js/2.e1c013c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a04881038ee41e7d9849",
+    "url": "/bhagavad-gita/static/js/main.f8c51fd7.chunk.js"
+  },
+  {
+    "revision": "47b154aecd5581a0da94",
+    "url": "/bhagavad-gita/static/js/runtime-main.fe393381.js"
+  },
+  {
+    "revision": "f33d9178c143ca4dace3dc4e7b8bd49e",
+    "url": "/bhagavad-gita/static/media/1.f33d9178.jpg"
+  },
+  {
+    "revision": "92fb17ecdfda04c14deb22b1175e31cf",
+    "url": "/bhagavad-gita/static/media/2.92fb17ec.jpg"
+  },
+  {
+    "revision": "7896245001dbc6883d17f7b6e412acac",
+    "url": "/bhagavad-gita/static/media/3.78962450.jpg"
+  },
+  {
+    "revision": "7eef6399163e94d847992e8f7f9797bb",
+    "url": "/bhagavad-gita/static/media/4.7eef6399.jpg"
+  },
+  {
+    "revision": "0bbfc705e37a927ce2ae72b749b3154d",
+    "url": "/bhagavad-gita/static/media/Simple-Line-Icons.0bbfc705.woff"
+  },
+  {
+    "revision": "6ac7320f709ffd2784b4a861e5b60395",
+    "url": "/bhagavad-gita/static/media/Simple-Line-Icons.6ac7320f.woff2"
+  },
+  {
+    "revision": "b2892aa62b0fb2c21c8d7700e2ef6e56",
+    "url": "/bhagavad-gita/static/media/Simple-Line-Icons.b2892aa6.ttf"
+  },
+  {
+    "revision": "dacd16c9a3d2cb61b472a35979015bf3",
+    "url": "/bhagavad-gita/static/media/Simple-Line-Icons.dacd16c9.eot"
+  },
+  {
+    "revision": "de8ad55bfeaebbe67c5b9a362749de1a",
+    "url": "/bhagavad-gita/static/media/Simple-Line-Icons.de8ad55b.svg"
+  },
+  {
+    "revision": "33d0b67c047e466b46c464b8eefc2d31",
+    "url": "/bhagavad-gita/static/media/custom-select.33d0b67c.png"
+  },
+  {
+    "revision": "48d61f016cb4f54bc87a9b89bf417532",
+    "url": "/bhagavad-gita/static/media/error-bg.48d61f01.jpg"
+  },
+  {
+    "revision": "071852ed416357e43362baac24183858",
+    "url": "/bhagavad-gita/static/media/fa-brands-400.071852ed.svg"
+  },
+  {
+    "revision": "0d2158700ccb68e527a6915a6f9256e3",
+    "url": "/bhagavad-gita/static/media/fa-brands-400.0d215870.woff"
+  },
+  {
+    "revision": "4019e2ef5746b8baa1ca57ff6afd6bed",
+    "url": "/bhagavad-gita/static/media/fa-brands-400.4019e2ef.woff2"
+  },
+  {
+    "revision": "913334b20fe18a3568d18d9178d2b9b8",
+    "url": "/bhagavad-gita/static/media/fa-brands-400.913334b2.ttf"
+  },
+  {
+    "revision": "b680adbac11d91675e2ecdb198206211",
+    "url": "/bhagavad-gita/static/media/fa-brands-400.b680adba.eot"
+  },
+  {
+    "revision": "190faaa2f9bcb3c7cf5d174fb7846ecc",
+    "url": "/bhagavad-gita/static/media/fa-regular-400.190faaa2.eot"
+  },
+  {
+    "revision": "4758ad6071911a36d5b4ea7faa9d3c16",
+    "url": "/bhagavad-gita/static/media/fa-regular-400.4758ad60.woff2"
+  },
+  {
+    "revision": "c794369e05c1bfbc3954539d6c12fb89",
+    "url": "/bhagavad-gita/static/media/fa-regular-400.c794369e.svg"
+  },
+  {
+    "revision": "da900afa8bd1d66d93fa576058d6a268",
+    "url": "/bhagavad-gita/static/media/fa-regular-400.da900afa.woff"
+  },
+  {
+    "revision": "dddf7b2cfdcc9f9da4354794809221c5",
+    "url": "/bhagavad-gita/static/media/fa-regular-400.dddf7b2c.ttf"
+  },
+  {
+    "revision": "0d995a145d7392132124440336bba586",
+    "url": "/bhagavad-gita/static/media/fa-solid-900.0d995a14.ttf"
+  },
+  {
+    "revision": "3997dfc8e6cb470ffb289421e7ab00c0",
+    "url": "/bhagavad-gita/static/media/fa-solid-900.3997dfc8.svg"
+  },
+  {
+    "revision": "4cb8ea72ad6d4f33465239dbc106e015",
+    "url": "/bhagavad-gita/static/media/fa-solid-900.4cb8ea72.eot"
+  },
+  {
+    "revision": "7960713e96c6058336d3928be08067a4",
+    "url": "/bhagavad-gita/static/media/fa-solid-900.7960713e.woff"
+  },
+  {
+    "revision": "9f3c8f805668d4182d2173b660a7a21e",
+    "url": "/bhagavad-gita/static/media/fa-solid-900.9f3c8f80.woff2"
+  },
+  {
+    "revision": "555ab3a8551d59f676ec01d27363265d",
+    "url": "/bhagavad-gita/static/media/img1.555ab3a8.jpg"
+  },
+  {
+    "revision": "b97c99cd78922b2c8579733a07c4a27b",
+    "url": "/bhagavad-gita/static/media/img2.b97c99cd.jpg"
+  },
+  {
+    "revision": "798035df60fbab3152494ce02b1ed029",
+    "url": "/bhagavad-gita/static/media/img3.798035df.jpg"
+  },
+  {
+    "revision": "e77a5906215f6eff13555472012cc060",
+    "url": "/bhagavad-gita/static/media/img4.e77a5906.jpg"
+  },
+  {
+    "revision": "73a034c48f5d4f176c56b39a1561172d",
+    "url": "/bhagavad-gita/static/media/img5.73a034c4.jpg"
+  },
+  {
+    "revision": "13ac84445b9c12938606cab8da416b89",
+    "url": "/bhagavad-gita/static/media/img6.13ac8444.jpg"
+  },
+  {
+    "revision": "4fed03f1e0fb2169381382b5e4294d14",
+    "url": "/bhagavad-gita/static/media/materialdesignicons-webfont.4fed03f1.woff"
+  },
+  {
+    "revision": "8babd01821c68d173e0f3a50eb09272d",
+    "url": "/bhagavad-gita/static/media/materialdesignicons-webfont.8babd018.svg"
+  },
+  {
+    "revision": "a65b9561e6b00796ee6a34ea2c81e661",
+    "url": "/bhagavad-gita/static/media/materialdesignicons-webfont.a65b9561.ttf"
+  },
+  {
+    "revision": "b8695cc16b97f1bd97446651af325e6d",
+    "url": "/bhagavad-gita/static/media/materialdesignicons-webfont.b8695cc1.eot"
+  },
+  {
+    "revision": "cc3cf0bd5af9550e4d1836a730647860",
+    "url": "/bhagavad-gita/static/media/materialdesignicons-webfont.cc3cf0bd.woff2"
+  },
+  {
+    "revision": "f432cf7d6b5594ac3f6e66419654af7a",
+    "url": "/bhagavad-gita/static/media/sidebarbg.f432cf7d.png"
+  },
+  {
+    "revision": "18e551e9f189fc24d6e791f1e0ae6c5a",
+    "url": "/bhagavad-gita/static/media/slokas.18e551e9.yaml"
+  },
+  {
+    "revision": "21a14988afe40342c6dba09631c622d7",
+    "url": "/bhagavad-gita/static/media/stats.21a14988.png"
+  },
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/bhagavad-gita/static/media/themify.2c454669.eot"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/bhagavad-gita/static/media/themify.a1ecc3b8.woff"
+  },
+  {
+    "revision": "cba68f986e60df8c74f4a53c3e39595c",
+    "url": "/bhagavad-gita/static/media/themify.cba68f98.svg"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "/bhagavad-gita/static/media/themify.e23a7dca.ttf"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/bhagavad-gita/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/bhagavad-gita/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/bhagavad-gita/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/bhagavad-gita/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/bhagavad-gita/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
