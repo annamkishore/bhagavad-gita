d69ec88f98677240897f60c34672be05e6c3e9fe
Kishore Annam <annamkishore@gmail.com>
2021-12-17 17:34:08 +0530

    slokas yaml parsed

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,29 @@
-import stats from "./stats.png";
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import yaml from 'yaml';
+
 import './App.css';
+import stats from "./stats.png";
+import slokasFile from "./slokas.yaml";
 
 function App() {
-    let slokasYaml = "https://raw.githubusercontent.com/annamkishore/annamkishore.github.io/79a994859fb6dce438b079c56bebb099b1f02581/bhagavad-gita/slokas.yaml";
+    const [slokas, setSlokas] = useState(0);
+
+    useEffect(async () => {
+        const result = await axios(slokasFile);
+        setSlokas(result.data);
+        window.slokas = result.data
+        window.yaml = yaml
+
+        window.slokasJson = yaml.parse(slokas)
+    });
+
     return (
         <div className="App">
             <h2>భగవద్గీత</h2>
-            <h2> Chapter wise count <a href={slokasYaml}>Slokas link</a></h2>
+            <h2> Chapter wise count </h2>
             <img src={stats}/> <br/>
+            <div>{slokas}</div>
         </div>
     );
 }
