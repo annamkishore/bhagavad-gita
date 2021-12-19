52c1d7513fc3d14ef48777d1dcdbe5ac20218749
Kishore Annam <annamkishore@gmail.com>
2021-12-20 00:50:01 +0530

    start time generation logic

--- src/utils/Util.js
+++ src/utils/Util.js
@@ -28,3 +28,27 @@ export function toSeconds(duration) {
         return Number(minutes) * 60 + Number(seconds);
     }
 }
+
+function toTime(seconds) {
+    let mm = parseInt(seconds / 60)
+    let ss = seconds - (mm * 60)
+    return `${mm}:${ss}`
+}
+
+// example for 6th chapter: getStartTime(1, "00:23", 47, "10:06")
+// output, for 6th chapter, use in the audio-time.yaml
+function getStartTime(start, startTime, end, endTime) {
+    let startSec = toSeconds(startTime)
+    let endSec = toSeconds(endTime)
+    let diffSec = endSec - startSec
+
+    let diff = end - start
+    let avgSec = diffSec / diff
+    let arrCount = diff - 1
+
+    return Array(arrCount).fill(avgSec)
+        .map((num, i) => parseInt(startSec + num * (i + 1)))
+        .map(num => toTime(num))
+}
+
+// Array(18).fill(12.4).map((num,i) => parseInt(3356+num *(i+1))).map(num => toTime(num))
