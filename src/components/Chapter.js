58229872ea0f896f9d5718fb22e36aa0aadb65c8
Kishore Annam <annamkishore@gmail.com>
2021-12-19 13:46:56 +0530

    audio playing upto 1.10

--- src/components/Chapter.js
+++ src/components/Chapter.js
@@ -1,14 +1,50 @@
 import React, {useState, useEffect} from 'react';
 import audio1 from "../resources/audio/Chapter-01-05.mp3";
+import {fetchAudioTime} from "../utils/Util";
 
 export function Chapter(props) {
     if (!props.slokas) {
         return <></>
     }
 
+    let timeData;
+    fetchAudioTime().then(data => {
+        timeData = data;
+    });
+
     let chapName = props.name
     let chapSlokas = props.slokas
     let slokas = Object.values(chapSlokas)
+
+    let isPlaying = false;
+    let currentSloka;
+    function playPause(num) {
+        console.log(timeData.audio[1][num])
+
+        if(!currentSloka || currentSloka !== num) {
+            currentSloka = num;
+
+            let url = document.querySelector("#songSource").src;
+            let songWindow = "#t=466,486";
+
+            url = url.split("#")[0] + songWindow;
+            document.querySelector("#songSource").src = url;
+            console.log(currentSloka, songWindow)
+            song.pause();
+            song.currentTime = parseInt(timeData.audio[1][num])
+            song.play();
+            isPlaying = true;
+            return;
+        }
+
+        if(isPlaying) {
+            song.pause();
+            isPlaying = false;
+        }else {
+            song.play();
+            isPlaying = true;
+        }
+    }
 
     return <>
         <table border={0} cellSpacing={0} cellPadding={0} align={"center"}
@@ -18,7 +54,7 @@ export function Chapter(props) {
                 <td colSpan={2} style={{textAlign: "center", backgroundColor: "grey"}}>
                     <span style={{fontSize: "1.5rem"}}>{chapName}</span>
                     <audio id="song" preload="auto" /*controls*/>
-                        <source src={audio1 + "#t=595"} type="audio/mp3"/>
+                        <source id="songSource" src={audio1 + "#t=466"} type="audio/mp3"/>
                         Your browser does not support the audio element.
                     </audio>
                 </td>
@@ -32,7 +68,8 @@ export function Chapter(props) {
                         {i + 1}
                     </td>
                     {/*   sloka   */}
-                    <td style={{paddingLeft: "0.5rem", borderLeft: 0, backgroundColor: i%2 ? "grey" : ""}}>
+                    <td style={{paddingLeft: "0.5rem", borderLeft: 0, backgroundColor: i%2 ? "grey" : ""}}
+                        onClick={()=>{playPause(i+1)}}>
                         {/*{sloka}*/}
                         {sloka.split("\\n").map((line, temp) =>
                             <span key={temp}>
--- src/utils/Util.js
+++ src/utils/Util.js
@@ -1,9 +1,16 @@
 import axios from "axios";
 import slokasFile from "../resources/slokas.yaml";
+import audioTimeFile from "../resources/audio/audio-time.yaml";
 import yaml from "yaml";
 
 export async function fetchData() {
     const response = await axios(slokasFile);
+    let responseObj = yaml.parse(response.data)
+    return responseObj;
+}
+
+export async function fetchAudioTime() {
+    const response = await axios(audioTimeFile);
     let responseObj = yaml.parse(response.data)
     return responseObj;
 }
